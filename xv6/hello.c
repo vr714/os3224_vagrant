a33a8db7c39b7ee44364265d6ed93c0fc933d403
vr714 <35711547+vr714@users.noreply.github.com>
2018-09-16 21:47:18 -0400

    Add files via upload

---
+++ xv6/hello.c
@@ -0,0 +1,14 @@
+
+//  Created by Viola Rreza on 9/14/18.
+//  Copyright © 2018 Viola Rreza. All rights reserved.
+//
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main() {
+    char string[] = "Hello World";
+    printf(1, "%s\n", string);
+    exit();
+}
